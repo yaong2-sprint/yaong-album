6ea81bb78e02dd78b0074f6571e1e8b458736945
OhSeungYoung <osy0056@khu.ac.kr>
2021-06-26 19:37:46 +0900

    style: Rename data to state

--- src/components/ResultSection.js
+++ src/components/ResultSection.js
@@ -1,31 +1,31 @@
 import CardView from './CardView.js';
 
 export default class ResultSection {
-  constructor({ $target, data = null }) {
+  constructor({ $target, state = null }) {
     this.$section = document.createElement('section');
     this.$section.className = 'result-section';
-    this.data = data;
+    this.state = state;
     $target.appendChild(this.$section);
 
     this.render();
   }
 
-  setState(data) {
-    this.data = data;
+  setState(state) {
+    this.state = state;
     this.render();
   }
 
   render() {
     this.$section.innerHTML = '';
-    if (this.data === null) {
+    if (this.state === null) {
       const $initialResult = document.createElement('div');
       $initialResult.innerHTML = '<h1>고양이 품종을 입력해주세요.</h1>';
       $initialResult.className = 'initial-result';
       this.$section.appendChild($initialResult);
-    } else if (this.data.length) {
+    } else if (this.state.length) {
       const cardView = new CardView({
         $target: this.$section,
-        data: this.data,
+        state: this.state,
       });
     } else {
       const $noResult = document.createElement('div');
